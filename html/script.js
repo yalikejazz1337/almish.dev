1cf20d3a97acc0da2802015d54dead8fd971d9b4
yalikejazz1337 <94409075+yalikejazz1337@users.noreply.github.com>
2023-09-18 17:13:39 -0700

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,10 +1,1 @@
-var pos1 = window.scrollY;
-window.onscroll = function () {
-  var pos2s = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-50px";
-  }
-  pos1 = pos2;
-};
+
